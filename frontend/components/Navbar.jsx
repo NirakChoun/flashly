9618e5f936b7e12becec2bc9ac6c8fdc612d6d57
NirakChoun <nirakchoun@gmail.com>
2025-01-30 20:25:45 -0600

    Add files

---
+++ frontend/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../src/assets/images/logo.svg";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="bg-slate-600 text-white">HelloA</nav>
+    </>
+  );
+};
+
+export default Navbar;
