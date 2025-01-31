67076b6683825982981f6446e071a4b2acf90a51
NirakChoun <nirakchoun@gmail.com>
2025-01-30 20:42:32 -0600

    Change files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import Navbar from "../components/Navbar";
-import "./index.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import Logo from "../src/assets/images/logo.svg";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="bg-slate-600 text-red-500">HelloA</nav>
+      <p className="bg-yellow-500">Yellow</p>
+      <p className="bg-red-500">Red</p>
+    </>
+  );
+};
+
+export default Navbar;
